9b78393f5a84aa5a8c8e70d2ffd038d0317380ae
Kai Davenport <kaiyadavenport@gmail.com>
2015-10-24 15:17:32 +0200

    basic interface for the database module

---
+++ src/api/database.js
@@ -0,0 +1,32 @@
+function create_project(data, done){
+
+}
+
+function view_project(id, done){
+
+}
+
+function list_projects(done){
+
+}
+
+function delete_project(id, done){
+
+}
+
+function save_project(id, data, done){
+
+}
+
+function list_images(done){
+
+}
+
+module.exports = {
+  create_project:create_project,
+  view_project:view_project,
+  list_projects:list_projects,
+  delete_project:delete_project,
+  save_project:save_project,
+  list_images:list_images
+}
