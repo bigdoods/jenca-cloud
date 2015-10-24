0d0669d361cf1b1b4e9336b9a93d52e6602695dc
Kai Davenport <kaiyadavenport@gmail.com>
2015-10-24 21:23:01 +0200

    basic messagebus implementation

---
+++ src/api/messagebus.js
@@ -0,0 +1,21 @@
+var Transport = require('./messagetransport')
+
+module.exports = function(type, opts){
+
+  if(!type) type = 'memory'
+    
+  var transport = Transport(type, opts)
+
+  function publish(channel, data, done){
+    transport.publish(channel, data, done){
+  }
+
+  function subscribe(channel, done){
+    transport.subscribe(channel, done){
+  }
+
+  return {
+    publish:publish,
+    subscribe:subscribe
+  }
+}
