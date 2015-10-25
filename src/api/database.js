00104122eb6ccf5b2bffe91f137023686bb2d3ee
Kai Davenport <kaiyadavenport@gmail.com>
2015-10-25 11:12:40 +0100

    add a waterfall to the database create project so it can pass the project data onwasrds

--- src/api/database.js
+++ src/api/database.js
@@ -1,6 +1,7 @@
 var Storage = require('./databasestorage')
 var MessageBus = require('./messagebus')
 var Messages = require('./messages')
+var async = require('async');
 
 module.exports = function(type, bus, opts){
 
@@ -10,13 +11,19 @@ module.exports = function(type, bus, opts){
   var messages = Messages(bus)
 
   function create_project(userid, data, done){
-    storage.create_project(userid, data, function(err, project){
-      if(err) return done(err)
 
-      messages.create_project(userid, data, function(err){
-        if(err) return done(err)
-        done(null, project)
-      })
+    var project = null
+    async.waterfall([
+      function(next){
+        storage.create_project(userid, data, next)
+      },
+      function(projectdata, next){
+        project = projectdata
+        messages.create_project(userid, project, next)
+      }
+    }], function(err){
+      if(err) return done(err)
+      done(null, project)
     })
   }
 
