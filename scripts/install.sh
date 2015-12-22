9deba0c5fc7471be68465112a8c78f3d38a00e0d
Kai Davenport <kaiyadavenport@gmail.com>
2015-12-22 16:42:21 +0000

    k8s 1.1.1 (so we get all the nice new stuff)

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@
 set -e
 # this is the script vagrant will use to provision the box
 mkdir -p /opt/bin
-curl https://storage.googleapis.com/kubernetes-release/release/v1.0.1/bin/linux/amd64/kubectl --silent --output /opt/bin/kubectl
+curl https://storage.googleapis.com/kubernetes-release/release/v1.1.1/bin/linux/amd64/kubectl --silent --output /opt/bin/kubectl
 chmod a+x /opt/bin/kubectl
 ln -s /home/core/share/scripts/jencactl /opt/bin/jencactl
