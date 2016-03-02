5776b309328fa3be4ea4bafaa90e825eef98b26f
px3l <connor@jenca.io>
2016-03-02 12:57:15 +0000

    basic K8S manifests for apps

---
+++ stack/manifests/apps/bimserver/run.sh
@@ -0,0 +1,6 @@
+docker run -d \
+    -e TOMCAT_USER=xxx \
+    -e TOMCAT_PASSWORD=xxx \
+    -p 8080:8080 \
+    --restart=always \
+    jenca/docker-bimserver
---
+++ stack/manifests/apps/ghost/run.sh
@@ -0,0 +1,1 @@
+docker run --name some-ghost -p 2368:2368 -d ghost
