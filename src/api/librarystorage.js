80361f1b60c580ba6026c8cb3e7a143642b98922
Kai Davenport <kaiyadavenport@gmail.com>
2016-03-02 18:30:36 +0000

    remove the old src code now we have refactored into services

--- src/api/librarystorage.js
+++
@@ -1,9 +0,0 @@
-var drivers = {
-  jsonfile:require('./library-drivers/jsonfile')
-}
-
-module.exports = function(type, opts){
-  var Driver = drivers[type]
-  var driver = Driver(opts)
-  return driver
-}
--- src/api/messagetransport.js
+++
@@ -1,9 +0,0 @@
-var drivers = {
-  memory:require('./message-drivers/memory')
-}
-
-module.exports = function(type, opts){
-  var Driver = drivers[type]
-  var driver = Driver(opts)
-  return driver
-}
