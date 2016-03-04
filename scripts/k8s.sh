4730a896b8d80851e73ea687ba9be4578d00810a
Kai Davenport <kaiyadavenport@gmail.com>
2016-03-04 17:22:28 +0000

    working commands to start kubernetes

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-export K8S_VERSION=${K8S_VERSION:="1.1.1"}
+export K8S_VERSION=${K8S_VERSION:="1.0.1"}
+export ETCD_VERSION=${ETCD_VERSION:="2.0.12"}
 
 usage() {
 cat <<EOF
@@ -11,6 +12,7 @@ k8s.sh hyperkube
 k8s.sh start
 k8s.sh stop
 k8s.sh restart
+k8s.sh download
 k8s.sh help
 EOF
 	exit 1
@@ -25,38 +27,42 @@ grepstop-container() {
 
 # start the etcd docker container
 cmd-etcd() {
-	docker run -d \
-	  --name etcd \
-	  --net host \
-	  kubernetes/etcd:2.0.5.1 \
-	  /usr/local/bin/etcd \
-	  	--addr=127.0.0.1:4001 \
-	  	--bind-addr=0.0.0.0:4001 \
-	  	--data-dir=/var/etcd/data
+	docker run \
+		--net=host \
+		-d \
+		gcr.io/google_containers/etcd:${ETCD_VERSION} /usr/local/bin/etcd \
+			--addr=127.0.0.1:4001 \
+			--bind-addr=0.0.0.0:4001 \
+			--data-dir=/var/etcd/data
 }
 
 # start the various kubernetes containers via the kyperkube
 cmd-hyperkube() {
-	docker run -d \
-		--net host \
-		-v /var/run/docker.sock:/var/run/docker.sock \
-		gcr.io/google_containers/hyperkube:v${K8S_VERSION} \
-		/hyperkube kubelet \
-			--api_servers=http://localhost:8080 \
-			--v=2 \
-			--address=0.0.0.0 \
-			--enable_server \
-			--hostname_override=127.0.0.1 \
-			--config=/etc/kubernetes/manifests
+	docker run \
+    --volume=/:/rootfs:ro \
+    --volume=/sys:/sys:ro \
+    --volume=/dev:/dev \
+    --volume=/var/lib/docker/:/var/lib/docker:ro \
+    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+    --volume=/var/run:/var/run:rw \
+    --net=host \
+    --pid=host \
+    --privileged=true \
+    -d \
+    gcr.io/google_containers/hyperkube:v${K8S_VERSION} /hyperkube kubelet \
+    	--containerized \
+    	--hostname-override="127.0.0.1" \
+    	--address="0.0.0.0" \
+    	--api-servers=http://localhost:8080 \
+    	--config=/etc/kubernetes/manifests
 }
 
 # start the kubernetes proxy (we are running the kubelet on this node)
 cmd-proxy() {
 	docker run -d \
-		--net host \
+		--net=host \
 		--privileged \
-		gcr.io/google_containers/hyperkube:v${K8S_VERSION} \
-		/hyperkube proxy \
+		gcr.io/google_containers/hyperkube:v${K8S_VERSION} /hyperkube proxy \
 			--master=http://127.0.0.1:8080 \
 			--v=2
 }
@@ -85,10 +91,17 @@ cmd-restart() {
 	cmd-start
 }
 
+cmd-download() {
+	echo "downloading kubectl..."
+	curl https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl --silent --output /usr/local/bin/kubectl
+	chmod a+x /usr/local/bin/kubectl
+}
+
 main() {
 	case "$1" in
 	etcd)					        shift; cmd-etcd; $@;;
 	hyperkube)					  shift; cmd-hyperkube; $@;;
+	download)             shift; cmd-download; $@;;
 	proxy)					      shift; cmd-proxy; $@;;
 	start)					      shift; cmd-start; $@;;
 	stop)					        shift; cmd-stop; $@;;
