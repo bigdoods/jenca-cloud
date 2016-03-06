92604c37a6fe947cf8be8caea22ac3224a607eae
Kai Davenport <kaiyadavenport@gmail.com>
2016-03-06 21:38:41 +0000

    change api so you can start/stop one service/resource

--- scripts/jenca.sh
+++ scripts/jenca.sh
@@ -13,57 +13,92 @@ EOF
 	exit 1
 }
 
-cmd-k8s-service() {
+cmd-k8s() {
   local action="$1"
-  local dir="$2"
-  local servicename=`basename $dir`
-  echo "service $action: $servicename"
-  kubectl $action -f ${dir}/service.yml
+  local file="$2"
+  local servicename=`basename $file`
+
+  if [[ -f "$file" ]]; then
+    echo "service $action: $servicename"
+    kubectl $action -f $file
+  fi
 }
 
-cmd-k8s-controller() {
-  local action="$1"
-  local dir="$2"
-  local servicename=`basename $dir`
-  echo "controller $action: $servicename"
-  kubectl $action -f ${dir}/controller.yml
-}
-
+# loop over the manifests directory and apply $action
+# to each folder (service)
+# each service can have:
+#  * controller.yml
+#  * service.yml
+# this is optional per service
+#
+# to start the whole stack: k8s start
+# to start one service: k8s start jenca-router
+# to start one services resource: k8s start jenca-router controller
+# to start a resource of all services: k8s start * controller
+#
 cmd-k8s-loop() {
+  
   local action="$1"
-  local manifestdir="${DIR}/../stack/manifests/core/*"
-  for dir in $(ls -d $manifestdir); 
-  do
-    if [[ -d $dir ]]; then
-      cmd-k8s-service $action $dir
+  local service="$2"
+  local resource="$3"
+
+  local manifestdir="${DIR}/../stack/manifests/core"
+
+  if [[ "$service" == "*" ]]; then
+    service=""
+  fi
+
+  # you can pass a single service - k8s start jenca-router
+  if [[ -n "$service" ]]; then
+
+    # you can pass a single resources - k8s start jenca-router controller
+    if [[ -z "$resource" || "$resource"=="service" ]]; then
+      cmd-k8s $action "${manifestdir}/${service}/service.yml"
     fi
-  done
-  local manifestdir="${DIR}/../stack/manifests/core/*"
-  for dir in $(ls -d $manifestdir); 
-  do
-    if [[ -d $dir ]]; then
-      cmd-k8s-controller $action $dir
+
+    if [[ -z "$resource" || "$resource"=="controller" ]]; then
+      cmd-k8s $action "${manifestdir}/${service}/controller.yml"
     fi
-  done
+
+  else
+    for dir in $(ls -d "${manifestdir}/*"); 
+    do
+      if [[ -d $dir ]]; then
+        echo "here $dir"
+        if [[ -z "$resource" || "$resource"=="service" ]]; then
+          cmd-k8s $action "${dir}/${service}/service.yml"
+        fi
+      fi
+    done
+    
+    for dir in $(ls -d "${manifestdir}/*"); 
+    do
+      if [[ -d $dir ]]; then
+        if [[ -z "$resource" || "$resource"=="controller" ]]; then
+          cmd-k8s $action "${dir}/${service}/controller.yml"
+        fi
+      fi
+    done
+  fi
 }
 
 cmd-start() {
-  cmd-k8s-loop create
+  cmd-k8s-loop create $@
 }
 
 cmd-replace() {
-  cmd-k8s-loop replace
+  cmd-k8s-loop replace $@
 }
 
 cmd-stop() {
-  cmd-k8s-loop delete
+  cmd-k8s-loop delete $@
 }
 
 main() {
 	case "$1" in
-	start)					      shift; cmd-start; $@;;
-	stop)					        shift; cmd-stop; $@;;
-  replace)              shift; cmd-replace; $@;;
+	start)					      shift; cmd-start $@;;
+	stop)					        shift; cmd-stop $@;;
+  replace)              shift; cmd-replace $@;;
 	*)                    usage $@;;
 	esac
 }
