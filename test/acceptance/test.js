b345f0c6a8982b3c26dc3221db23a395fe9ed12b
Kai Davenport <kaiyadavenport@gmail.com>
2016-03-09 22:11:04 +0000

    check we can speak to /v1/projects

--- test/acceptance/test.js
+++ test/acceptance/test.js
@@ -2,17 +2,26 @@ var tape = require('tape')
 var async = require('async')
 var hyperrequest = require('hyperrequest')
 
-function get(url, done){
+var routerPort = null
+
+function get(host, url, done){
   hyperrequest({
-    url:'http://127.0.0.1:8080' + url,
+    url:host + url,
     method:'GET',
   }, done)
 }
 
+function k8sget(url, done){
+  return get('http://127.0.0.1:8080', url, done)
+}
+
+function routerget(url, done){
+  return get('http://127.0.0.1:' + routerPort, url, done)
+}
+
 tape('can connect to the k8s api', function (t) {
   
-  
-  get('/version', function(err, res){
+  k8sget('/version', function(err, res){
     if(err){
       t.error(err)
       return t.end()
@@ -26,7 +35,7 @@ tape('can connect to the k8s api', function (t) {
 
 tape('can read the exposed port for the route', function (t) {
 
-  get('/api/v1/namespaces/default/services/jenca-router-public', function(err, res){
+  k8sget('/api/v1/namespaces/default/services/jenca-router-public', function(err, res){
     if(err){
       t.error(err)
       return t.end()
@@ -34,7 +43,22 @@ tape('can read the exposed port for the route', function (t) {
 
     t.equal(typeof(res.body.spec.ports[0].nodePort), 'number', 'the node port is a number')
     
+    routerPort = res.body.spec.ports[0].nodePort
+
     t.end()
   })
 
+})
+
+tape('can read /v1/projects/version', function (t) {
+  routerget('/v1/projects/version', function(err, res){
+    if(err){
+      t.error(err)
+      return t.end()
+    }
+
+    t.equal(res.body.match(/^\d+\.\d+\.\d+$/) ? true : false, true, 'the version is a semver')
+
+    t.end()
+  })
 })
